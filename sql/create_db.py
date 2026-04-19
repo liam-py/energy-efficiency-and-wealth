b7827d574a302dcd405ff9823880d559689f067f
Liam Yates <liampy@bu.edu>
2026-04-18 22:43:07 -0400

    switched to using pickle files to preserve data types. Going to revisit fetch_data and processing scripts to fetch and process PLUTO data.

--- sql/create_db.py
+++ sql/create_db.py
@@ -5,4 +5,7 @@ connection = sqlite3.connect('energy_efficiency_and_wealth.db') # create a new d
 cursor = connection.cursor() # create a cursor object we can use to interact with our database connection
 
 # import data!
-ll84_df = pd.read_csv('/data/clean/ll84_cleaned.csv')
+ll84_df = pd.read_pickle('../data/clean/ll84_cleaned.pkl')
+acs_df = pd.read_pickle('../data/clean/acs_cleaned.pkl')
+
+# write dataframes to SQL db
